bd6b0c069204f405ad9e54e7ff7306b21aee89b0
Kinga <278428@student.pwr.edu.pl>
2025-11-27 16:00:32 +0100

    added other needed tests and change the name of the file for DrugInfoService because the name was wrong and wasn't count into tests

--- medtrackerapp/tests/test_services.py
+++ medtrackerapp/tests/test_services.py
@@ -4,7 +4,7 @@ from medtrackerapp.models import DrugInfoService
 
 class TestDrugInfoService(unittest.TestCase):
 
-    @patch("medtrackerapp.models.requests.get")
+    @patch("medtrackerapp.services.requests.get")
     def test_fetch_drug_info(self, mock_get):
         mock_get.return_value = Mock(status_code=200)
         mock_get.return_value.json.return_value = {
@@ -20,3 +20,20 @@ class TestDrugInfoService(unittest.TestCase):
         self.assertEqual(result["manufacturer"], "McKesson")
         self.assertEqual(result["warnings"], ["Keep out of reach of children."])
         self.assertEqual(result["purpose"], ["Pain reliever"])
+
+    def test_drug_name_empty(self):
+        with self.assertRaises(ValueError):
+            DrugInfoService.get_drug_info("")
+
+    @patch("medtrackerapp.services.requests.get")
+    def test_api_non_200(self, mock_get):
+        mock_get.return_value = Mock(status_code=500)
+        with self.assertRaises(ValueError):
+            DrugInfoService.get_drug_info("Ibuprofen")
+
+    @patch("medtrackerapp.services.requests.get")
+    def test_no_results_found(self, mock_get):
+        mock_get.return_value = Mock(status_code=200)
+        mock_get.return_value.json.return_value = {"results": []}
+        with self.assertRaises(ValueError):
+            DrugInfoService.get_drug_info("Ibuprofen")
